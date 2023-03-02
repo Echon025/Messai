8893d67b91a7c27d0bfbf8b0ccb2588d3f9de4d4
Echon25 <szymonjasinski685@gmail.com>
2023-03-02 13:46:00 +0100

    khkh

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const showText = document.querySelector('.textarea')
-const btn = Document.querySelector('.btn-submit')
-function textShow() {
-	showText.classList.toggle('btn-submit')
-	if (showText.classList.contains('btn-submit')) {
-    } 
-}
-btn.addEventListener('click', textShow)
